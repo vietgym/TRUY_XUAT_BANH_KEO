5b2723a0eaa1c6729b6fde42dba739326dbe47e8
vietgym <vietgym007@gmail.com>
2023-12-23 11:32:23 +0700

    feat: upload file alembic

---
+++ alembic/versions/9d5a8bf96952_initial_user.py
@@ -0,0 +1,33 @@
+"""initial user
+
+Revision ID: 9d5a8bf96952
+Revises: 1ef2b30b6223
+Create Date: 2023-12-21 21:05:54.724893
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '9d5a8bf96952'
+down_revision: Union[str, None] = '1ef2b30b6223'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.create_table(
+        "users",
+        sa.Column("userID", sa.String, primary_key=True, index=True),
+        sa.Column("userEmail", sa.String),
+        sa.Column("userLg", sa.String),
+        sa.Column("userPass", sa.String),
+        sa.Column("role", sa.String),
+    )
+
+
+def downgrade() -> None:
+    op.drop_table("users")
